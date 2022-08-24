10c2f865f51bb29c931f345217168706433991ac
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2022-08-24 22:59:37 +0200

    Add main app window

--- NaplatnaRampaProjekat/MainWindow/MainWindow.xaml.cs
+++ NaplatnaRampaProjekat/MainWindow/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace NaplatnaRampaProjekat.MainWindow
+namespace MainWindow
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
