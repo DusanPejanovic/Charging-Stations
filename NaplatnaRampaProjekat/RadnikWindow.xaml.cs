b3d217367694dd6792a1776c5cdc1e0f53bf1e6a
DusanPejanovic <95129566+DusanPejanovic@users.noreply.github.com>
2022-08-25 23:42:24 -0700

    gotova fizicka napalata

--- NaplatnaRampaProjekat/RadnikWindow.xaml.cs
+++ NaplatnaRampaProjekat/RadnikWindow.xaml.cs
@@ -1,23 +1,9 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NaplatnaRampaProjekat
 {
-    /// <summary>
-    /// Interaction logic for RadnikWindow.xaml
-    /// </summary>
     public partial class RadnikWindow : Window
     {
         public RadnikWindow()
@@ -37,37 +23,51 @@ namespace NaplatnaRampaProjekat
         {
 
             string cs = @"server=localhost;userid=root;password=admin;database=Naplatne_Rampe_DB";
+            string idCena = "";
 
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string sql = "SELECT Datum_Pocetka_vazenja, Datum_Zavrsetka_vazenja, Cena FROM Cena WHERE ID_Cena in (  SELECT ID_Cena FROM Cenovnik WHERE ID_Stanica_pocetna IN (SELECT ID_Stanica FROM Stanica WHERE Naziv= '" + pocetnaStanicaTextBox.Text + "') AND  ID_Stanica_krajnja IN (SELECT ID_Stanica FROM Stanica WHERE Naziv= '" + krajnjaStanicaTextBox.Text + "') AND  ID_Kategorija_vozila IN (SELECT ID_Kategorija_vozila FROM Kategorija_vozila WHERE Naziv_kategorije='" + kategorijaVozilaTextBox.Text  + "'));";
+            string sql = "SELECT Datum_Pocetka_vazenja, Datum_Zavrsetka_vazenja, Cena, ID_Cena FROM Cena WHERE ID_Cena in (  SELECT ID_Cena FROM Cenovnik WHERE ID_Stanica_pocetna IN (SELECT ID_Stanica FROM Stanica WHERE Naziv= '" + pocetnaStanicaTextBox.Text + "') AND  ID_Stanica_krajnja IN (SELECT ID_Stanica FROM Stanica WHERE Naziv= '" + krajnjaStanicaTextBox.Text + "') AND  ID_Kategorija_vozila IN (SELECT ID_Kategorija_vozila FROM Kategorija_vozila WHERE Naziv_kategorije='" + kategorijaVozilaTextBox.Text  + "'));";
             using var cmd = new MySqlCommand(sql, con);
+
 
             using MySqlDataReader rdr = cmd.ExecuteReader();
 
-            bool nadjen = false;
+
 
             while (rdr.Read())
             {
-                string pass = rdr.GetString(1);
-                string user = rdr.GetString(0);
+                DateTime datumPocetkaVazenja = rdr.GetDateTime(0);
 
 
-                // Trace.WriteLine(user);
-                // Trace.WriteLine(korisnickoImeTextBox.Text);  korisno za ispisivanje
+                DateTime datumZavrsetkaVazenja = DateTime.Now.AddDays(1);
 
-                if (pass == pocetnaStanicaTextBox.Text && user == kategorijaVozilaTextBox.Text)
+                if (rdr.IsDBNull(1) == false) 
+                    datumZavrsetkaVazenja = rdr.GetDateTime(1);
+
+
+                Double cena = rdr.GetDouble(2);
+               
+
+                if (DateTime.Now >  datumPocetkaVazenja && DateTime.Now < datumZavrsetkaVazenja)
                 {
-
-                    nadjen = true;
-                    MessageBox.Show("Dobro dosao ti si admin", "Notifikacija", MessageBoxButton.YesNoCancel);
+                    idCena = rdr.GetString(3);
+                    MessageBox.Show("Naplati " + cena.ToString() + " dinara.", "Notifikacija", MessageBoxButton.OK);
+                    MessageBox.Show("Obavljenja transakcija", "Notifikacija", MessageBoxButton.OK);
                     break;
                 }
             }
 
             rdr.Close();
+
+
+            using var cmd2 = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = " INSERT INTO Transakcija (Datum_Transakcije, Registraciona_tablica, ID_Stanica_pocetna, ID_Stanica_krajnja, ID_Kategorija_vozila, ID_Cena) VALUES( NOW(), '" + registracionaTablicaTextBox.Text + "', (SELECT ID_Stanica  FROM Stanica WHERE Naziv='" + pocetnaStanicaTextBox.Text + "' ), (SELECT ID_Stanica FROM Stanica WHERE Naziv='" + krajnjaStanicaTextBox.Text + "' ), (SELECT ID_Kategorija_vozila FROM Kategorija_vozila WHERE Naziv_kategorije='" + kategorijaVozilaTextBox.Text + "'), " + idCena + ");";
+            cmd.ExecuteNonQuery();
         }
+
 
     }
 }
